bdb9f870c30dbb2a59e11603bd195b27e074e34d
Poundpitak Amornlerdlittichai <Damon1FX@gmail.com>
2023-04-13 18:57:46 +0700

    :money_with_wings: update noti

--- noti_discord.php
+++ noti_discord.php
@@ -1,8 +1,8 @@
 <?php
-  $webhook_url = 'https://discord.com/api/webhooks/1095956977198317588/bw08WWE7NX_IDBHmLQj9B96iDtLi2QSGSczDsABkYxqOD6oAbv5Seb9ehivrimPEDuMz';
+  $webhook_url = 'https://discord.com/api/webhooks/1096039523605041192/rgP3USmbVpNJMqLwbnuvgV_1sVoRyQuF80i2Q1fA6qtx-ALsUWXd_QcX1HsJH9FQzqhh';
 
 
-  $message = array('content' => 'AI KAK BASUKE BASUKE STUPID!!!!!');
+  $message = array('content' => 'AI KAK BASUKE STUPID!!!!!');
 
   $curl = curl_init($webhook_url);
   curl_setopt($curl, CURLOPT_HTTPHEADER, array('Content-Type: application/json'));
