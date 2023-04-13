417b3eee816c663fa3693f2c9613e949e0f802bf
Poundpitak Amornlerdlittichai <Damon1FX@gmail.com>
2023-04-13 15:11:54 +0700

    Update noti_discord.php

--- noti_discord.php
+++ noti_discord.php
@@ -1,7 +1,7 @@
 <?php
   $webhook_url = 'https://discord.com/api/webhooks/1095956977198317588/bw08WWE7NX_IDBHmLQj9B96iDtLi2QSGSczDsABkYxqOD6oAbv5Seb9ehivrimPEDuMz';
 
-  $message = array('content' => 'AI KAK BASYKE!!!!!');
+  $message = array('content' => 'AI KAK BASUKE!!!!!');
 
   $curl = curl_init($webhook_url);
   curl_setopt($curl, CURLOPT_HTTPHEADER, array('Content-Type: application/json'));
