db3a447a786ce726c885017cb74ee4c0bbe67b62
Ubuntu <ubuntu@ip-172-31-29-235.ap-southeast-1.compute.internal>
2023-04-13 08:19:17 +0000

    message

--- noti_discord.php
+++ noti_discord.php
@@ -1,7 +1,11 @@
 <?php
   $webhook_url = 'https://discord.com/api/webhooks/1095956977198317588/bw08WWE7NX_IDBHmLQj9B96iDtLi2QSGSczDsABkYxqOD6oAbv5Seb9ehivrimPEDuMz';
 
+<<<<<<< HEAD
   $message = array('content' => 'AI KAK BASUKE!!!!!');
+=======
+  $message = array('content' => 'AI KAK BASYKE!!!!!');
+>>>>>>> acdc4d2 (message)
 
   $curl = curl_init($webhook_url);
   curl_setopt($curl, CURLOPT_HTTPHEADER, array('Content-Type: application/json'));
